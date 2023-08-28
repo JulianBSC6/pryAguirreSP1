a56071037bcdb46e14082dac5277060130cbe4ea
root <mgiraudo@ies21.edu.ar>
2023-08-28 14:08:36 -0300

    Continuar codigo

---
+++ pryAguirreSP1/frmMenu.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace pryAguirreSP1
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.txtDatos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(27, 387);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(102, 23);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear archivo";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(197, 386);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer archivo";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(345, 355);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(102, 23);
+            this.btnGrabar.TabIndex = 2;
+            this.btnGrabar.Text = "Grabar archivo";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.Location = new System.Drawing.Point(113, 74);
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.Size = new System.Drawing.Size(215, 20);
+            this.txtDatos.TabIndex = 3;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDatos);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnCrear);
+            this.Name = "frmMenu";
+            this.Text = "Crear archivos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.TextBox txtDatos;
+    }
+}
+
---
+++ pryAguirreSP1/frmMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pryAguirreSP1
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            StreamWriter swCrearArchivo = new StreamWriter("ArchivoPrueba", false);
+
+            if( swCrearArchivo != null)
+            {
+                MessageBox.Show("Archivo creado");
+
+            }
+
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            StreamWriter swManejoArchivo = new StreamWriter("ArchivoPrueba", false)
+
+                try
+                {
+                    swManejoArchivo.WriteLine(txtDatos.Text);
+                    swManejoArchivo.Close();
+                    MessageBox.Show("Almacenado");
+                    txtDatos.Text = "";
+                    txtDatos.Focus();
+                }
+                catch (Exception)
+                {
+                MessageBox.Show("Fatal error");
+                }
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
